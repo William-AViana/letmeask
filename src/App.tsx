6540e18acf2ece55b304fe1a0a9bd529065a2e37
William Viana <willviana.batera@gmail.com>
2021-06-24 15:59:08 -0300

    add const unsubscribe for authenticator user

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   const [user, setUser] = useState<User>();
 
   useEffect(() => {
-    auth.onAuthStateChanged(user => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
       if (user) {
         const { displayName, photoURL, uid } = user
 
