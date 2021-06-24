b994264886913e9f7fe5d0bc26234a003a2b0539
William Viana <willviana.batera@gmail.com>
2021-06-23 22:49:29 -0300

    new import Link from react-router-dom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
 import ilustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
-import '../styles/auth.scss'
+
 import { Button } from '../components/Button';
+
+import '../styles/auth.scss'
 
 export function NewRoom() {
   return (
@@ -26,7 +29,7 @@ export function NewRoom() {
             </Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">Clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
           </p>
         </div>
       </main>
