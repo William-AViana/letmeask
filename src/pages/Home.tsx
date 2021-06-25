87f4eb866bf9823c917c751e45da781543160ac1
William Viana <willviana.batera@gmail.com>
2021-06-24 22:25:26 -0300

    add function for onSubmit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -54,7 +54,7 @@ export function Home() {
             Crie sua sala com o google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
