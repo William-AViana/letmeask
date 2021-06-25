f9930ad16413b20a727b26fe749b41ca067dc8ab
William Viana <willviana.batera@gmail.com>
2021-06-25 18:16:40 -0300

    add import components, image and style

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,3 +1,9 @@
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from "../components/Button";
+
+import '../styles/room.scss';
+
 export function Room() {
   return (
     <h1>Sala</h1>
