d5c414e6fc9dd868d0fb12e3da12ff34cc7a30b9
William Viana <willviana.batera@gmail.com>
2021-06-24 22:24:59 -0300

    create function for handle join room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,6 +22,23 @@ export function Home() {
     history.push('/rooms/new');
   }
 
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists');
+      return;
+    }
+    history.push(`/rooms/${roomCode}`);
+  }
+
+
   return (
     <div id="page-auth">
       <aside>
