30a30fede4f384ae26bf83524f6bfc57804da87c
William Viana <willviana.batera@gmail.com>
2021-06-24 21:57:58 -0300

    add tag Switch from routers

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" exact component={NewRoom} />
-        <Route path="/rooms/:id" component={Room} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" exact component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
