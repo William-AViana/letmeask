67f04e2f4e274e70d8adbf77f44cf4112f9fe86a
William Viana <willviana.batera@gmail.com>
2021-06-24 22:22:49 -0300

    import database

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { FormEvent, useState } from 'react';
 import ilustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
-
+import { database } from '../services/firebase';
 import { useAuth } from './../hooks/useAuth';
 import { Button } from '../components/Button';
 
